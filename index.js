488226f4828663baadb17f0f2ccc3c18f281fbdc
ReklatsMasters <dmitrycvet@gmail.com>
2016-01-24 02:33:46 +0500

    init

---
+++ index.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const VivaldiPosition = require('./lib/vivaldiposition');
+const HeightCoordinates = require('./lib/heightcoodinates');
+
+/**
+ * Vivaldi Papers :
+ * https://www.cs.umd.edu/class/spring2007/cmsc711/papers/vivaldi.pdf
+ */
+
+function create(data) {
+	if (data instanceof Float32Array && data.length == 4) {
+		return VivaldiPosition.fromFloatArray(data);
+	} else if (isCoordinate(data)) {
+		return new VivaldiPosition(data);
+	} else {
+		return VivaldiPosition.create(data);
+	}
+}
+
+function update(rtt, p1, p2) {
+	if (!isPosition(p1) || !positionOrCoordinate(p2)) {
+		throw new TypeError('Argument 1 and argument 2 must be position or coordinate');	
+	}
+	
+	return p1.update(rtt, p2);
+}
+
+function distance(p1, p2) {
+	if (!isPosition(p1) || !positionOrCoordinate(p2)) {
+		throw new TypeError('Argument 1 and argument 2 must be position or coordinate');	
+	}
+	
+	return p1.estimateRTT(p2);
+}
+
+function equals(p1, p2) {
+	if (positionOrCoordinate(p1)) {
+		return p1.equals(p2);
+	}
+	
+	return false;
+}
+
+/** @private */
+function isPosition(p) {
+	return p instanceof VivaldiPosition;
+}
+
+/** @private */
+function isCoordinate(c) {
+	return c instanceof HeightCoordinates;	
+}
+
+/** @private */
+function positionOrCoordinate(data) {
+	return isPosition(data) || isCoordinate(data);
+}
+
+module.exports = {
+	create,
+	update,
+	distance,
+	equals,
+	VivaldiPosition,
+	HeightCoordinates
+}
---
+++ readme.md
@@ -0,0 +1,36 @@
+# vivaldi-coordinates
+Vivaldi: A Decentralized Network Coordinate System. Originaly description [here](https://www.cs.umd.edu/class/spring2007/cmsc711/papers/vivaldi.pdf)
+
+## API
+
+##### `create(data: Float32Array): VivaldiPosition`
+Create `VivaldiPosition` instance from raw coordinates. Argument `data` should have length == 4.
+
+##### `create(data: HeightCoordinates): VivaldiPosition`
+Create `VivaldiPosition` instance from `HeightCoordinates` instance.
+
+##### `create([error: Number]): VivaldiPosition`
+Create new empty `VivaldiPosition` instance. Argument `error` is optional.
+
+##### `update(rtt: Number, p1: VivaldiPosition, p2: VivaldiPosition|HeightCoordinates): bool`
+Update position `p1` with other position `p2` and time `rtt`.
+
+##### `distance(p1: VivaldiPosition, p2: VivaldiPosition|HeightCoordinates): number`
+Calculate distace between `p1` and `p2`.
+
+##### `equals(p1, p2): bool`
+Check to equals `p1` and `p2`.
+
+## Example
+```js
+const vivaldi = require('vivaldi-coordinates');
+
+var local_pos = vivaldi.create();	// create new empty pos;
+var remote_pos;	// position from some remote host
+var rtt = 7;	  // ping time to remote host
+
+vivaldi.update(rtt, local_pos, remote_pos);	// update local position
+```
+
+## License
+MIT, 2015 (c) Dmitry Tsvettsikh
